af0cfd89043049b91e8c4d38b03c34196a95f8bc
JOHANN AMAYA LOPEZ <Johannpez99@gmail.com>
2022-07-10 17:59:28 -0500

    Documentación de librería
    adicionar el carácter Ñ

--- bookshop.py
+++ bookshop.py
@@ -1,3 +1,5 @@
+# Diccionario de abecedario a morse
+
 dicctm = {
     'A': '.-',
     "B": "-...",
@@ -13,6 +15,7 @@ dicctm = {
     "L": ".-..",
     "M": "--",
     "N": "-.",
+    "Ñ": "--.--",
     "O": "---",
     "P": ".--.",
     "Q": "--.-",
@@ -43,6 +46,8 @@ dicctm = {
     "  ": ".-.-.-"
 }
 
+# Diccionario de binario a morse
+
 diccbm = {
     "01000001": ".-",
     "01000010": "-...",
@@ -58,6 +63,7 @@ diccbm = {
     "01001100": ".-..",
     "01001101": "--",
     "01001110": "-.",
+    "11010001": "--.--",
     "01001111": "---",
     "01010000": ".--.",
     "01010001": "--.-",
@@ -83,8 +89,10 @@ diccbm = {
     "": " "
 }
 
-
 def espacio(x):
+
+    #Funcion que agrega un espcio entre cada frase
+
     lista, fra = [], ""
     for i in range(len(x)):
         if x[i] != " ":
@@ -97,18 +105,26 @@ def espacio(x):
 
 
 def texto_morse():
+
+    #Funcion para traducir de Texto a morse
+
     new = ""
     x = list(input("Ingrese una frase:   ").upper())
     for i in range(len(x)):
         new += dicctm[x[i]] + " "
     print(">>> La traduccion a Morse es")
-    print(new)
+    return(new)
 
 
 def morse_texto():
+
+    #Funcion para traducir de morse a texto
+    print("ejemplo de binario")
+    print(".... --- .-.. .-   -- . .-.. ..")
     list_of_key = list(dicctm.keys())
     list_of_value = list(dicctm.values())
     new = ""
+
     x = list(input("Ingrese el Codigo Morse:   "))
     x = espacio(x)
     for i in range(len(x)):
@@ -117,10 +133,13 @@ def morse_texto():
         position = list_of_value.index(x[i])
         new += list_of_key[position]
     print(">>> La traduccion a Texto es")
-    print(new)
+    return(new)
 
 
 def morse_bits():
+
+    #Funcion para traducir de morse a bits
+
     list_of_key = list(diccbm.keys())
     list_of_value = list(diccbm.values())
     new = ""
@@ -129,11 +148,14 @@ def morse_bits():
         position = list_of_value.index(x[i])
         new += list_of_key[position]
     print(">>> La traduccion a Bits es")
-    print(new)
+    return(new)
 
 
 def bits_morse():
-    print("ejemplo de binario")
+
+    #Funcion para traducir de bits a morse
+
+    print("Ejemplo de binario")
     print("00000000110110110011100000111111000111111001111110000000111011111111011101110000000110001111110000000000111111001111110000000110000110111111110111011100000011011100000000000")
     x = list(input("Ingrese el Binario:   "))
     morse, cont, cont_0 = "", 0, 0
@@ -155,10 +177,13 @@ def bits_morse():
             elif cont_0 > 6:
                 morse += ".-.-.-"
                 cont_0 = 0
-
-    print(morse)
+    print(">>> La traduccion de Bits a morse es:")
+    return(morse)
 
 def txt_bits_morse():
+
+    #Funcion para traducir de Texto a bits a morse
+
     list_of_key = list(diccbm.keys())
     list_of_value = list(diccbm.values())
     new, new_2, new_3 = "", "", ""
@@ -169,6 +194,5 @@ def txt_bits_morse():
         position = list_of_value.index(new_2)
         new += list_of_key[position]
     print(">>> La traduccion a Texto a Bits es:")
-    print(new)
-    print(">>> La traduccion a Bits a Morse es:")
-    print(new_3)
+    return(new,new_3)
+
